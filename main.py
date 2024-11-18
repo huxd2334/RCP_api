d20c3de353f9e747739da2be19a6890a204b7613
huxd2334 <131602633+huxd2334@users.noreply.github.com>
2024-11-18 23:49:15 +0700

    Update main.py final line

--- main.py
+++ main.py
@@ -145,3 +145,4 @@ async def predict(data: PredictionRequest):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+# ê chưa chạy, t chưa train lại model để đẩy dô, nào ta upload r dô test mậy
